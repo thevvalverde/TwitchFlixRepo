9840d0ad0e4000c45561b548a0647b9d70594671
thevvalverde <fvalverde007@gmail.com>
2021-03-29 13:48:49 +0100

    Implemented authentication

--- app/src/main/java/com/felipe/twitchflix/LoginActivity.java
+++ app/src/main/java/com/felipe/twitchflix/LoginActivity.java
@@ -1,16 +1,24 @@
 package com.felipe.twitchflix;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     Button mLoginButton;
@@ -18,11 +26,17 @@ public class LoginActivity extends AppCompatActivity {
     EditText mPassword;
     String mUserText = "";
     String mPassText = "";
+    final String TAG = "TwitchFlix";git c
+
+    // Firebase instances variables
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         mLoginButton = findViewById(R.id.login_button);
         mUsername = findViewById(R.id.username);
@@ -32,20 +46,45 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(TextUtils.isEmpty(mUsername.getText())){
+                mUserText = mUsername.getText().toString();
+                mPassText = mPassword.getText().toString();
+                if(mUserText.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Please insert a valid username or email!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(TextUtils.isEmpty(mPassword.getText())){
+                if(mPassText.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Please insert your password!", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 // TODO
                 // CREDENTIAL CHECKING
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                tryLogin(mUserText, mPassText);
+
+                //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                //startActivity(intent);
             }
         });
+
+    }
+
+    private void tryLogin(String user, String pass) {
+        mFirebaseAuth.signInWithEmailAndPassword(mUserText, mPassText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "Login successful.");
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                            return;
+                        }
+                        else {
+                            Log.d(TAG, "Login Failed.");
+                            Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/felipe/twitchflix/MainActivity.java
+++ app/src/main/java/com/felipe/twitchflix/MainActivity.java
@@ -1,5 +1,6 @@
 package com.felipe.twitchflix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -7,20 +8,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.felipe.twitchflix.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String ANONYMOUS = "Anonymous";
     static ViewHolder mViewHolder = new ViewHolder();
     private DrawerLayout mDrawer;
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        if (mFirebaseAuth.getCurrentUser() == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
         setContentView(R.layout.main_drawer);
+        setNavigationViewListener();
 
 
         this.mViewHolder.watchButton = findViewById(R.id.watch_button);
@@ -54,8 +72,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.logout:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                break;
+        }
+        return true;
+    }
+
     private static class ViewHolder {
         Button watchButton;
         Button streamButton;
+    }
+
+    private String getUserName() {
+        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        if (user != null) {
+            return user.getDisplayName();
+        }
+
+        return ANONYMOUS;
+    }
+
+    private void signOut() {
+        mFirebaseAuth.signOut();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
