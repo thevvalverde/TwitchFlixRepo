c49738f181ff2156d5b11fcf7c043e2e50617b15
thevvalverde <fvalverde007@gmail.com>
2021-03-29 20:01:47 +0100

    Implemented account creation and unique username checking

--- app/src/main/java/com/felipe/twitchflix/CreateAccountActivity.java
+++ app/src/main/java/com/felipe/twitchflix/CreateAccountActivity.java
@@ -1,16 +1,60 @@
 package com.felipe.twitchflix;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
 
 public class CreateAccountActivity extends AppCompatActivity {
     Button mCreateButton;
@@ -18,18 +62,26 @@ public class CreateAccountActivity extends AppCompatActivity {
     EditText mMailText;
     EditText mPassText;
     EditText mPassCheckText;
+
+    // Firebase instances variables
     FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase;
+    DatabaseReference myUserRef;
 
     private String mUser;
     private String mMail;
     private String mPass;
     private String mPassCheck;
 
+    final String TAG = "TwitchFlix";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mDatabase = FirebaseDatabase.getInstance();
+        myUserRef = mDatabase.getReference();
         mAuth = FirebaseAuth.getInstance();
         mCreateButton = findViewById(R.id.create_account_button);
         mUserText = findViewById(R.id.user_edit_text);
@@ -57,9 +109,54 @@ public class CreateAccountActivity extends AppCompatActivity {
                     Toast.makeText(CreateAccountActivity.this, "Please make sure you typed the same password twice!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                myUserRef.child("users").orderByChild("username").equalTo(mUser).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()) {
+                            Toast.makeText(CreateAccountActivity.this, "This username is already taken!", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            tryToCreate();
+                        }
+                    }
 
-            }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) { }
+                });
+                }
+
         });
 
     }
+
+    public void tryToCreate() {
+        mAuth.createUserWithEmailAndPassword(mMail, mPass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()) {
+                            try {
+                                throw task.getException();
+                            }
+                            catch (FirebaseAuthUserCollisionException existEmail)
+                            {
+                                Toast.makeText(CreateAccountActivity.this, "This email is already linked!", Toast.LENGTH_SHORT).show();
+                            }
+                            catch (Exception e)
+                            {
+                                Log.d(TAG, "onComplete: " + e.getMessage());
+                            }
+                        } else {
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(mUser)
+                                    .build();
+                            myUserRef.child("users").child(mUser).setValue(new User(mUser, mMail));
+                            startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
+                            finish();
+                            return;
+                        }
+                    }
+                });
+    }
+
 }
