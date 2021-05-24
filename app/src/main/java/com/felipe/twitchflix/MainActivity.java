3dc80827eb1e94b25a899396963b3bbc5ec797e4
thevvalverde <fvalverde007@gmail.com>
2021-05-24 09:46:32 +0100

    Updated icon and moved setText to Main UI thread

--- app/src/main/java/com/felipe/twitchflix/MainActivity.java
+++ app/src/main/java/com/felipe/twitchflix/MainActivity.java
@@ -5,30 +5,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.InputType;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 import static com.felipe.twitchflix.LoginActivity.KEY_CHECKBOX;
 import static com.felipe.twitchflix.LoginActivity.SHAREDPREF_KEY;
@@ -80,7 +70,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         NavigationView navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
         TextView navUsername = headerView.findViewById(R.id.nav_header_title);
-        navUsername.setText(mUsername);
+
+        // For some reason this doesn't work
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                navUsername.setText(mUsername);
+            }
+        });
+        // navUsername.setText(mUsername);
+
         this.mViewHolder.watchButton = findViewById(R.id.watch_button);
         this.mViewHolder.streamButton = findViewById(R.id.stream_button);
         this.mViewHolder.deleteAccount = findViewById(R.id.delete_account);
